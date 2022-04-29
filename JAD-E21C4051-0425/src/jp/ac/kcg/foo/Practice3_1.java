4ccf26402a45fded3e80384960b6018a982e2d39
“masaki1215” <masaki12150872@gmail.com>
2022-04-29 21:50:56 +0900

    Practice3_1a commit

--- JAD-E21C4051-0425/src/jp/ac/kcg/foo/Practice3_1.java
+++ JAD-E21C4051-0425/src/jp/ac/kcg/foo/Practice3_1.java
@@ -20,9 +20,9 @@ public class Practice3_1 extends javafx.application.Application {
 		Scene scene = new Scene(borderPane, 200, 100);//
 		stage.setScene(scene); // シーンをステージへ追加
 		stage.show(); // ステージを表示
-		
+
 		inputForm.setOnAction((e) -> {
-			outputArea.setText(outputArea.getText()+inputForm.getText());
+			outputArea.setText(outputArea.getText() + inputForm.getText());
 		});
 	}
 
